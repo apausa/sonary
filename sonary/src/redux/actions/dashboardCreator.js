6dcfe35de80381f5193a3b65d87083e68c749a8a
pabloapausa <pabloapausa@outlook.com>
2021-10-24 13:27:28 +0200

    Key

--- sonary/src/redux/actions/dashboardCreator.js
+++ sonary/src/redux/actions/dashboardCreator.js
@@ -4,10 +4,10 @@ import actionTypes from './actionTypes';
 
 export function loadDashboard() {
   return async (dispatch) => {
-    const response = await fetch(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=callback&page_size=15&country=wx&f_has_lyrics=1&apikey=${process.env.API_KEY}`);
-    let mod = await response.text();
-    mod = await mod.substring(9, (mod.length - 2));
-    const dashboard = await JSON.parse(mod).message.body.track_list;
+    const response = await fetch('https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=callback&page_size=15&country=wx&f_has_lyrics=1&apikey=8ba90f0cc799ec2725591826448e1d42');
+    let mod = await response?.text();
+    mod = await mod?.substring(9, (mod.length - 2));
+    const dashboard = await JSON.parse(mod)?.message?.body?.track_list;
     dispatch({
       type: actionTypes.LOAD_DASHBOARD,
       dashboard
--- sonary/src/redux/reducers/dashboardReducer.js
+++ sonary/src/redux/reducers/dashboardReducer.js
@@ -3,7 +3,7 @@ import actionTypes from '../actions/actionTypes';
 export default function dashboardReducer(state = [], action) {
   let dashboard = state;
   if (action.type === actionTypes.LOAD_DASHBOARD) {
-    dashboard = action.dashboard.map(({ track }) => track);
+    dashboard = action?.dashboard?.map(({ track }) => track);
   }
   return dashboard;
 }
