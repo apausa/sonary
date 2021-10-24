eb8ef7186f0f431bd45c8ca29124d103daa95e20
pabloapausa <pabloapausa@outlook.com>
2021-10-24 13:07:26 +0200

    Hide API key

--- sonary/src/redux/actions/dashboardCreator.js
+++ sonary/src/redux/actions/dashboardCreator.js
@@ -4,7 +4,7 @@ import actionTypes from './actionTypes';
 
 export function loadDashboard() {
   return async (dispatch) => {
-    const response = await fetch('https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=callback&page_size=15&country=wx&f_has_lyrics=1&apikey=8ba90f0cc799ec2725591826448e1d42');
+    const response = await fetch(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=callback&page_size=15&country=wx&f_has_lyrics=1&apikey=${process.env.API_KEY}`);
     let mod = await response.text();
     mod = await mod.substring(9, (mod.length - 2));
     const dashboard = await JSON.parse(mod).message.body.track_list;
--- sonary/src/redux/actions/detailsCreator.js
+++ sonary/src/redux/actions/detailsCreator.js
@@ -4,7 +4,7 @@ import actionTypes from './actionTypes';
 
 export default function loadDetails(track_id) {
   return async (dispatch) => {
-    const response = await fetch(`https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=callback&track_id=${track_id}&apikey=8ba90f0cc799ec2725591826448e1d42`);
+    const response = await fetch(`https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=callback&track_id=${track_id}&apikey=${process.env.API_KEY}`);
     let mod = await response.text();
     mod = await mod.substring(9, (mod.length - 2));
     const details = await JSON.parse(mod).message.body.lyrics;
